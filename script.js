ee44cb32db3898ffb1f9c0db0efd366c44723f85
mac <mac@MacBook-Pro-mac.local>
2023-08-09 13:29:17 +0200

    first commit on mac

---
+++ script.js
@@ -0,0 +1,29 @@
+const sizeUpBtn = document.querySelector('.sizeUp')
+const sizeDownBtn = document.querySelector('.sizeDown')
+const colorBtn = document.querySelector('.color')
+const paragraph = document.querySelector('p')
+
+let fontSize = 36
+
+const sizeUp = () => {
+	if (fontSize >= 48) return
+	fontSize += 3
+	paragraph.style.fontSize = fontSize + 'px'
+}
+const sizeDown = () => {
+	if (fontSize <= 21) return
+
+	fontSize -= 3
+	paragraph.style.fontSize = fontSize + 'px'
+}
+const color = () => {
+	const r = Math.floor(Math.random() * 255)
+	const g = Math.floor(Math.random() * 255)
+	const b = Math.floor(Math.random() * 255)
+
+	paragraph.style.color = `rgb(${r},${g},${b})`
+}
+
+sizeUpBtn.addEventListener('click', sizeUp)
+sizeDownBtn.addEventListener('click', sizeDown)
+colorBtn.addEventListener('click', color)
